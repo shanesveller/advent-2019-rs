840e3552e74d1e9c9db3c8a2142c721ce020300d
Shane Sveller <shanesveller@gmail.com>
2019-12-02 20:37:05 -0600

    Add day 2 part 2 solution

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -62,11 +62,46 @@ fn main() -> io::Result<()> {
     let filename = Path::new(&args[1]);
     let input = fs::read_to_string(filename)?;
 
-    let mut nums = day_2::parse_input(&input);
-    nums[1] = 12;
-    nums[2] = 2;
+    let clean_nouns = day_2::parse_input(&input);
 
-    dbg!(part_1(nums));
+    {
+        let noun = 12;
+        let verb = 2;
+        let mut nums: Vec<usize> = vec![clean_nouns[0], noun, verb];
+        nums.extend_from_slice(&clean_nouns[3..]);
+
+        nums[1] = noun;
+        nums[2] = verb;
+
+        dbg!(part_1(nums));
+    }
+
+    {
+        let mut noun = 1;
+        let mut verb = 1;
+        let mut iterations = 0;
+        let target = 19_690_720;
+
+        'outer: for noun_candidate in 1..99 {
+            for verb_candidate in 1..99 {
+                iterations += 1;
+                let mut nums: Vec<usize> = vec![clean_nouns[0], noun_candidate, verb_candidate];
+                nums.extend_from_slice(&clean_nouns[3..]);
+                nums[1] = noun_candidate;
+                nums[2] = verb_candidate;
+
+                let output = part_1(nums);
+
+                if output == target {
+                    noun = noun_candidate;
+                    verb = verb_candidate;
+                    break 'outer;
+                }
+            }
+        }
+
+        dbg!(100 * noun + verb, iterations);
+    }
 
     Ok(())
 }
